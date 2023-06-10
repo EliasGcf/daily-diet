9e5dda99251d9391d3c42d6663012638bfeada6f
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-06-10 14:23:22 -0300

    chore(mobile): setup expo-router

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,6 +1,9 @@
+process.env.EXPO_ROUTER_APP_ROOT = '../../src/app';
+
 module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [require.resolve("expo-router/babel")],
   };
 };
