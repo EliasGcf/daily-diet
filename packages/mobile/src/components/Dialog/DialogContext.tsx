966fed9dfd5325830b74b6404e7d98edbe9fd6f9
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-06-25 14:50:59 -0300

    chore(mobile): allow to pass props to dialog root

--- packages/mobile/src/components/Dialog/DialogContext.tsx
+++ packages/mobile/src/components/Dialog/DialogContext.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, createContext, useMemo, useState } from 'react';
+import { createContext, useCallback, useContext, useMemo, useState } from 'react';
 
 type DialogContextProps = {
   isOpen: boolean;
@@ -8,16 +8,37 @@ type DialogContextProps = {
 
 const DialogContext = createContext({} as DialogContextProps);
 
-export function DialogProvider({ children }: Required<PropsWithChildren>) {
+type DialogProviderProps = {
+  children: React.ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+};
+
+export function DialogProvider({
+  open: openFromProps,
+  onOpenChange,
+  children,
+}: DialogProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenChange = useCallback(
+    (newValue: boolean) => {
+      setIsOpen(newValue);
+
+      if (onOpenChange) {
+        onOpenChange(openFromProps !== undefined ? !openFromProps : newValue);
+      }
+    },
+    [onOpenChange, openFromProps],
+  );
 
   const value = useMemo(
     () => ({
-      isOpen,
-      onOpenChange: (open: boolean) => setIsOpen(open),
-      handleClose: () => setIsOpen(false),
+      isOpen: openFromProps ?? isOpen,
+      onOpenChange: handleOpenChange,
+      handleClose: () => handleOpenChange(false),
     }),
-    [isOpen],
+    [handleOpenChange, isOpen, openFromProps],
   );
 
   return <DialogContext.Provider value={value}>{children}</DialogContext.Provider>;
@@ -26,7 +47,7 @@ export function DialogProvider({ children }: Required<PropsWithChildren>) {
 DialogProvider.displayName = 'DialogProvider';
 
 export function useDialog() {
-  const context = React.useContext(DialogContext);
+  const context = useContext(DialogContext);
 
   if (!context) {
     throw new Error('useDialog must be used within a DialogProvider');
