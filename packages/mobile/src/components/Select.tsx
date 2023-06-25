42ff00bdec063cbcae261bc7ef20c55a686c86f5
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-06-25 13:51:21 -0300

    fix(mobile): select styles at android

--- packages/mobile/src/components/Select.tsx
+++ packages/mobile/src/components/Select.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
 import { theme } from '../shared/theme';
@@ -16,33 +17,40 @@ export function Select({ value, selected = false, ...rest }: SelectProps) {
       {...rest}
       underlayColor={value ? theme.colors.green.light : theme.colors.red.light}
       activeOpacity={1}
-      style={[
-        baseStyles.container,
-        selected && {
-          borderColor: value ? theme.colors.green.dark : theme.colors.red.dark,
-          backgroundColor: value ? theme.colors.green.light : theme.colors.red.light,
-        },
-      ]}
+      style={styles.container}
     >
-      <Dot size={8} color={value ? theme.colors.green.dark : theme.colors.red.dark} />
-      <Text size="sm" weight="bold" color="gray.100">
-        {value ? 'Sim' : 'Não'}
-      </Text>
+      <View
+        style={[
+          styles.content,
+          selected && {
+            borderColor: value ? theme.colors.green.dark : theme.colors.red.dark,
+            backgroundColor: value ? theme.colors.green.light : theme.colors.red.light,
+          },
+        ]}
+      >
+        <Dot size={8} color={value ? theme.colors.green.dark : theme.colors.red.dark} />
+        <Text size="sm" weight="bold" color="gray.100">
+          {value ? 'Sim' : 'Não'}
+        </Text>
+      </View>
     </RectButton>
   );
 }
 
-const baseStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    paddingVertical: 16,
     backgroundColor: theme.colors.gray[600],
     borderRadius: 6,
+    flex: 1,
+  },
+  content: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: theme.colors.gray[600],
+    paddingVertical: 16,
     gap: 8,
-    flex: 1,
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: theme.colors.gray[600],
   },
 });
