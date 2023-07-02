9d6b584ac0c74fcb4598d35c16d627514e6f4a7f
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-07-02 18:20:54 -0300

    fix(mobile): only apply axios delay in dev

--- packages/mobile/src/lib/api.ts
+++ packages/mobile/src/lib/api.ts
@@ -4,7 +4,7 @@ export const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
 
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV === 'development') {
   api.interceptors.response.use(async (response) => {
     // eslint-disable-next-line no-promise-executor-return
     await new Promise((resolve) => setTimeout(resolve, 1000));
