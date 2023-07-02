89a85621e53ee1b47d48f0487a9f9d5bfe3dd5c7
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-07-02 13:02:53 -0300

    chore(mobile): add react-hook-form inputs to sign up

--- packages/mobile/src/app/(public)/signup.tsx
+++ packages/mobile/src/app/(public)/signup.tsx
@@ -1,22 +1,83 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Link } from 'expo-router';
+import { useForm } from 'react-hook-form';
 import { StyleSheet, View } from 'react-native';
+import { z } from 'zod';
 
 import LogoSvg from '@assets/logo.svg';
 
 import { Button } from '@components/Button';
+import { Form } from '@components/Form';
 import { KeyboardController } from '@components/KeyboardController';
-import { TextInput } from '@components/ui/TextInput';
+
+const formSchema = z
+  .object({
+    email: z
+      .string({ required_error: 'E-mail é obrigatório' })
+      .email('E-mail inválido')
+      .nonempty('E-mail é obrigatório'),
+    password: z
+      .string({ required_error: 'Senha é obrigatório' })
+      .nonempty('Senha é obrigatório'),
+    passwordConfirmation: z
+      .string({ required_error: 'Confirme a senha' })
+      .nonempty('Confirme a senha'),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'As senhas não coincidem',
+    path: ['passwordConfirmation'],
+  });
+
+type FormData = z.infer<typeof formSchema>;
 
 export default function SignUpPage() {
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function handleSubmit(formData: FormData) {
+    console.log('SignUpPage ~ formData:', formData);
+  }
+
   return (
     <KeyboardController>
       <View style={styles.container}>
         <LogoSvg width={300} height={60} />
         <View style={styles.content}>
-          <TextInput label="E-mail" placeholder="example@email.com" />
-          <TextInput placeholder="••••••••" label="Senha" secureTextEntry />
-          <TextInput placeholder="••••••••" label="Confirme a senha" secureTextEntry />
-          <Button title="Cadastrar" />
+          <Form.TextInput
+            label="E-mail"
+            name="email"
+            control={form.control}
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoComplete="email"
+            placeholder="example@email.com"
+            returnKeyType="default"
+            onSubmitEditing={() => form.handleSubmit(handleSubmit)()}
+          />
+          <Form.TextInput
+            name="password"
+            control={form.control}
+            placeholder="••••••••"
+            label="Senha"
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect={false}
+            secureTextEntry
+            onSubmitEditing={() => form.handleSubmit(handleSubmit)()}
+          />
+          <Form.TextInput
+            name="passwordConfirmation"
+            control={form.control}
+            placeholder="••••••••"
+            label="Senha"
+            autoCapitalize="none"
+            autoComplete="off"
+            autoCorrect={false}
+            secureTextEntry
+            onSubmitEditing={() => form.handleSubmit(handleSubmit)()}
+          />
+          <Button title="Cadastrar" onPress={() => form.handleSubmit(handleSubmit)()} />
           <Link asChild href="/login" replace>
             <Button variant="outline" title="Já tenho conta" />
           </Link>
